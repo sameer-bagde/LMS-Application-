03a66648c9aeb8a65930acfa648db4b8c193f639
NeUTRAL_oNE <bagdesameer92@gmail.com>
2023-11-15 20:50:10 +0530

    small views changes

--- migrations/20231107163152-create-chapter.js
+++ migrations/20231107163152-create-chapter.js
@@ -18,6 +18,9 @@ module.exports = {
       courseId: {
         type: Sequelize.INTEGER,
       },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
